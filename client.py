c500c20cefb06a32e4c87d030512271d908ffc93
Yaswanth Reddy Jonnala <104518732+yasreddi@users.noreply.github.com>
2022-11-25 23:30:33 +0530

    client.py
    client code

---
+++ client.py
@@ -0,0 +1,32 @@
+import socket
+ 
+Ip = socket.gethostbyname(socket.gethostname())
+FORMAT = "utf-8"
+Size = 1024
+Port = 8888
+Address = (Ip, Port)
+
+ 
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #starting the socket
+ 
+    client.connect(Address) #connecting to server
+
+    file = open("thefile.txt", "r") # opening the file
+    data = file.read()
+ 
+    client.send("thefile.txt".encode(FORMAT)) #send the filename to server
+    msg = client.recv(Size).decode(FORMAT)
+    print(f"[SERVER]: {msg}")
+ 
+    client.send(data.encode(FORMAT)) #receiving the data from the server
+    msg = client.recv(Size).decode(FORMAT)
+    print(f"[SERVER]: {msg}")
+
+    file.close() #closing the file
+ 
+    client.close() #disconnecting
+ 
+ 
+if __name__ == "__main__":
+    main()
